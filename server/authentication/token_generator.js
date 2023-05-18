d1a3006e9313bd2954f5d698afa2425a6f19e66a
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-18 05:37:47 +0530

    changed jwt algo from .env to direct implementation

--- server/authentication/token_generator.js
+++ server/authentication/token_generator.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
-
+require("dotenv").config();
 module.exports = {
   genAccess: async (claims) => {
     const token = await jwt.sign(claims, process.env.ACCESS_KEY, {
-      algorithm: process.env.JWT_ALGO,
+      algorithm: "HS256",
       expiresIn: process.env.AT_LIFE,
     });
     return token;
   },
   genRefresh: async (claims) => {
     const token = await jwt.sign(claims, process.env.REFRESH_KEY, {
-      algorithm: process.env.JWT_ALGO,
+      algorithm: "HS256",
       expiresIn: process.env.RT_LIFE,
     });
     return token;
--- server/authentication/verify.js
+++ server/authentication/verify.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { default: Redis } = require("ioredis");
+require("dotenv").config();
 const redis = new Redis();
 const {
   sendAccessCookie,
